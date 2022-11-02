770773502e9c6e9a29a337c82b8996cd5baf8147
plplmax <50287455+plplmax@users.noreply.github.com>
2022-11-02 16:40:52 +0300

    feat: implement a base routing
    feat: implement a base routing
    
    fix: fix eslint problems

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
+const app = express();
+const port = 3000;
+
+function response(methodName) {
+  return (req, res) => {
+    res.send(`Hello from the ${methodName} response`);
+  };
+}
+
+app.route('/')
+  .get(response('GET'))
+  .post(response('POST'))
+  .put(response('PUT'))
+  .delete(response('DELETE'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
